d250fa997f1982add3e541a6f40147f2ef8b8f8b
TomasSorgetti <tomassorgetti456@gmail.com>
2024-11-19 16:49:52 -0300

    feat: categories entity added

--- scripts/domains/category/category.service.js
+++ scripts/domains/category/category.service.js
@@ -1,9 +1,13 @@
 import { categories } from "../../data/index.js";
+import { Category } from "../../entities/categories.entity.js";
+
 export class CategoryService {
   #categories;
 
   constructor() {
-    this.#categories = categories;
+    this.#categories = categories.map(
+      (category) => new Category(category.id, category.name, category.image)
+    );
   }
 
   getAllCategories() {
---
+++ scripts/entities/categories.entity.js
@@ -0,0 +1,7 @@
+export class Category {
+  constructor(id, name, image) {
+    this.id = id;
+    this.name = name;
+    this.image = image;
+  }
+}
